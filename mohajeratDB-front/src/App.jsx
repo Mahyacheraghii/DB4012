fbd3bd45d0f9d19fc8b754d57f1aadd527ea9b82
MahyaCheraghi <mahya_ch@yahoo.com>
2024-06-30 06:15:22 +0330

    adding frontend

---
+++ mohajeratDB-front/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
